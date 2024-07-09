f05989ad76691dac605e59f116a76137d867a073
sheldon-hung <sheldon55071@gmail.com>
2024-07-09 23:28:22 +0800

    final version

---
+++ ALU_1bit.v
@@ -0,0 +1,16 @@
+module ALU_1bit( result, carryOut, a, b, invertA, invertB, operation, carryIn, less ); 
+  
+  output wire result;
+  output wire carryOut;
+  
+  input wire a;
+  input wire b;
+  input wire invertA;
+  input wire invertB;
+  input wire[1:0] operation;
+  input wire carryIn;
+  input wire less;
+  
+  /*your code here*/ 
+  
+endmodule
